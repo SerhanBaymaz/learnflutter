b9bb6addbb7e9a539c8b1f481659e25f6d9b406e
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2023-07-11 00:46:20 +0300

    Dice roller app has done.

--- dice_flutter/lib/main.dart
+++ dice_flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,24 +16,53 @@ void main() {
   ));
 }
 
-class DicePage extends StatelessWidget {
-  const DicePage({super.key});
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int diceLeft = Random().nextInt(5) + 1;
+  int diceRight = Random().nextInt(5) + 1;
 
   @override
   Widget build(BuildContext context) {
+    void changeLeftDice() {
+      diceLeft = Random().nextInt(5) + 1;
+    }
+
+    void changeRightDice() {
+      diceRight = Random().nextInt(5) + 1;
+    }
+
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Image.asset("images/dice1.png"),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    changeLeftDice();
+                  });
+                  print("Left button pressed.");
+                },
+                child: Image.asset("images/dice$diceLeft.png"),
+              ),
             ),
           ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Image.asset("images/dice2.png"),
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      changeRightDice();
+                    });
+                    print("Right button pressed.");
+                  },
+                  child: Image.asset("images/dice$diceRight.png")),
             ),
           ),
         ],
